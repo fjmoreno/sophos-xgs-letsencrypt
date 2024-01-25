dcda445def91261608aee27536b516fb8724efd7
Scriptkiddi <fritz@otlinghaus.it>
2024-01-25 16:22:57 +0100

    Fix setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 # Setup
-mkdir -p /conf/acme/http
 HOME=/conf/acme
-cd /conf/acme
+mkdir -p $HOME/http
+cd $HOME
 curl https://raw.githubusercontent.com/acmesh-official/acme.sh/master/acme.sh -o acme.sh
 chmod +x acme.sh
 curl https://raw.githubusercontent.com/helsinki-systems/sophos-xgs-letsencrypt/main/renew.sh -o renew.sh
@@ -10,5 +10,5 @@ chmod +x renew.sh
 curl https://raw.githubusercontent.com/helsinki-systems/sophos-xgs-letsencrypt/main/config.csv -o config.csv
 echo "Please insert letsencrypt account email"
 read email
-chmod +x /conf/acme/acme.sh
-/conf/acme/acme.sh --register-account -m $email --server letsencrypt
+chmod +x $HOME/acme.sh
+$HOME/acme.sh --home /conf/acme --register-account -m $email --server letsencrypt
